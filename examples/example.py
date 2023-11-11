35d382c80b59dcd170951fc61710c208f831adaa
Abhiram Shibu <abhiramshibu1998@gmail.com>
2023-11-12 00:45:30 +0530

    Examples: Usecase example
    Signed-off-by: Abhiram Shibu <abhiramshibu1998@gmail.com>

---
+++ examples/example.py
@@ -0,0 +1,22 @@
+import sys
+try:
+    from NoIp import NoIp
+except ModuleNotFoundError:
+    print("NoIp not found, install it by using pip install https://github.com/abhiramshibu/NoIp")
+    sys.exit(-1)
+
+hostname = input("Enter your hostname:")
+username = input("Enter your username:")
+password = input("Enter your password:")
+ipaddres = input("Enter the ip address you like to change it to:")
+
+dynDns = NoIp.DynDns()
+dynDns.setHostName(hostname)
+dynDns.setAuth(username,password)
+status = dynDns.update(ipaddres)
+
+if(status == 0):
+    print("Success!")
+else:
+    dynDnsStatus = NoIp.DynDnsStatus()
+    print(dynDnsStatus.statusToString(status))
