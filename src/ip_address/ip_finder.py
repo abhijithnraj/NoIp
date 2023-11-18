c2f83f75a7f38fc5ca45660f26e58cc4374ceb22
abhijithnraj <abhijithnrajg@gmail.com>
2023-11-18 11:11:12 +0530

    Moved RequestHandler to utils
    Signed-off-by: abhijithnraj <abhijithnrajg@gmail.com>

--- src/ip_address/ip_finder.py
+++ src/ip_address/ip_finder.py
@@ -1,42 +1,8 @@
 # Import the required modules
 import socket
-import requests
 import abc
 
-# from utils.request_handler import RequestHandler
-
-
-# Define a class for the request handler
-class RequestHandler:
-
-    # Initialize the request handler with a URL
-    def __init__(self, url):
-        self.url = url
-
-    # Define a method to send the request and get the response
-    def send_request(self):
-        # Try to make an HTTP request to the URL using the requests module
-        try:
-            response = requests.get(self.url)
-            # Check if the response status code is 200 (OK)
-            if response.status_code == 200:
-                # Return the response object
-                return response
-            else:
-                # Raise an exception if the response status code is not 200
-                raise Exception(f"Request failed with status code {response.status_code}")
-        # Catch any requests exceptions
-        except requests.exceptions.RequestException as e:
-            # Print the error message
-            print(f"Request error: {e}")
-            # Return None
-            return None
-        # Catch any other exceptions
-        except Exception as e:
-            # Print the error message
-            print(f"Other error: {e}")
-            # Return None
-            return None
+from utils.request_handler import RequestHandler
 
 # FIXME :  Add asynchronous methods 
 # Define an abstract base class for the IP finder strategies
